6971e7902ad133e5f32ab536214211e348b6aee5
Sven Slootweg <jamsoftgamedev@gmail.com>
2016-03-15 04:27:33 +0100

    Fixed default handling to not override optional arguments - I really should not be publishing modules while sleep-deprived...

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ const Promise = require("bluebird");
 
 module.exports = function(method, defaults = []) {
 	return function() {
+		var originalArgs = new Array(arguments.length);
+
+		for(var i = 0; i < originalArgs.length; ++i) {
+			originalArgs[i] = arguments[i];
+		}
+		
 		return new Promise((resolve, reject) => {
-			var args = new Array(Math.max(arguments.length, defaults.length) + 1);
+			var args = new Array(Math.max(originalArgs.length, defaults.length) + 1);
 			
-			for(var i = 0; i < args.length; ++i) {
-				args[i] = arguments[i];
+			for(var i = 0; i < originalArgs.length; ++i) {
+				args[i] = originalArgs[i];
 			}
 			
-			if (arguments.length - 1 < defaults.length) {
-				for(var i = arguments.length - 1; i < defaults.length; i++) {
+			if (originalArgs.length < defaults.length) {
+				for(var i = originalArgs.length; i < defaults.length; i++) {
 					if (defaults[i] != null) {
 						args[i] = defaults[i];
 					}
